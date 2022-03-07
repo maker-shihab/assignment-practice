470130495d2dd9ad316712dfdf6436fae4d57d82
Shihab <73121069+maker-shihab@users.noreply.github.com>
2022-03-07 19:52:28 +0600

    first problem solve

---
+++ problem.js
@@ -0,0 +1,10 @@
+// Problem solve one
+const anaToVori = ana => {
+  if(ana < 0){
+    alert('Sorry boss set perject Number');
+  }
+  else{
+    console.log(ana + 'Ana = ' + (ana / 16) +'Vori'); 
+  }
+}
+anaToVori(10);
