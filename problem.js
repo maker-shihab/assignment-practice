671167a48fda24cc99390400b651d5c040c84ea9
Shihab <73121069+maker-shihab@users.noreply.github.com>
2022-03-07 20:54:21 +0600

    Some Update

--- problem.js
+++ problem.js
@@ -1,13 +1,13 @@
 // Problem solve one
 const anaToVori = ana => {
-  if(ana < 0){
-    alert('Sorry boss set perject Number');
+  if(ana <= 0){
+    console.log('Sorry boss set perject Number');
   }
   else{
     console.log(ana + 'Ana = ' + (ana / 16) +'Vori'); 
   }
 }
-anaToVori(10);
+anaToVori(0);
 // Problem Two
 const pandaCost = (singara, somucha, jilapi) => {
   const singaraPrice = singara * 7;
@@ -17,3 +17,23 @@ const pandaCost = (singara, somucha, jilapi) => {
    console.log(`Total MRP = ${totalPrice}tk`);
 }
 pandaCost(2, 0, 2);
+
+// Picnic Budget 
+function pricnicBudget(mamber) {
+    if(mamber < 0 ){
+      console.log('Sorry boss set Right Number');
+    }
+    else if(mamber > 0 && mamber <= 100){
+      const result = mamber * 5000;
+      console.log(result);
+    } 
+    else if(mamber > 100 && mamber <= 200){
+      const result = mamber * 4000;
+      console.log(result);
+    } 
+    else if(mamber > 200 && mamber <= 300){
+      const result = mamber * 3000;
+      console.log(result);
+    }
+}
+pricnicBudget(101);
