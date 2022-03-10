b3b41f37145a9ac67e48b10cb032bfcb5c252b8c
Shihab <73121069+maker-shihab@users.noreply.github.com>
2022-03-11 01:00:40 +0600

    update code

--- setTimeOut/setTimeOut.js
+++ setTimeOut/setTimeOut.js
@@ -1,1 +1,17 @@
 // This is for set Time Out Practice
+const setTime = document.getElementById('btn-one');
+const setTimeTwo = document.getElementById('btn-two');
+const setTimeOut = document.getElementById('btn-three');
+setTime.addEventListener('click', (e) =>{
+  e.preventDefault();
+  saveData();
+});
+function saveData(){
+  const message = document.getElementById('warning-one');
+  message.innerText = 'Success your are selected';
+  message.classList.add('btn');
+  setTimeOut(() => {
+    message.innerText = '';
+    message.classList.remove('btn');
+  }, 3000)
+}
